b291cf0834c3c89b7202ca7737f0637facffe6e3
jonsoku2 <whdtjr2792@gmail.com>
2020-04-11 21:19:13 +0900

    installing slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React dependencies.
+import React, { FC, useEffect, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
 
-function App() {
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+const App: FC = ({ children }) => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState<any[]>([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
+  // Render the Slate context.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
+      <Editable />
+    </Slate>
   );
-}
-
+};
 export default App;
