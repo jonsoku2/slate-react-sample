18cc002af3cc12e61086fbd31c0b4e984cd9e715
jonsoku2 <whdtjr2792@gmail.com>
2020-04-11 21:27:41 +0900

    Defining Custom Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // Import React dependencies.
-import React, { FC, useEffect, useMemo, useState } from "react";
+import React, { FC, useEffect, useCallback, useMemo, useState } from "react";
 // Import the Slate editor factory.
-import { createEditor } from "slate";
+import { createEditor, Transforms, Editor } from "slate";
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
@@ -14,16 +14,37 @@ const App: FC = ({ children }) => {
       children: [{ text: "A line of text in a paragraph." }],
     },
   ]);
+
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   // Render the Slate context.
   return (
     <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
       <Editable
+        renderElement={renderElement}
         onKeyDown={(event) => {
-          if (event.key === "&") {
-            // Prevent the ampersand character from being inserted.
+          console.log(event.key);
+          if (event.key === "`" && event.ctrlKey) {
             event.preventDefault();
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText("and");
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match]: any = Editor.nodes(editor, {
+              match: (n) => n.type === "code",
+            });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              { match: (n) => Editor.isBlock(editor, n) }
+            );
           }
         }}
       />
@@ -31,3 +52,16 @@ const App: FC = ({ children }) => {
   );
 };
 export default App;
+
+const DefaultElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
+
+// Define a React component renderer for our code blocks.
+const CodeElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
